2ed602656af76f31670aa9a14d0fca079d27208c
thiagoanalytics <thiagoromualdo.nascimento@gmail.com>
2025-08-19 15:41:11 -0300

    Configurando Prefect

---
+++ dags/pipeline.py
@@ -0,0 +1,7 @@
+#No Prefect, os principais blocos são:
+    #Flow → Representa sua pipeline inteira.
+    #Task → São os passos individuais dentro da pipeline (ex: extrair dados, transformar, carregar).
+    #Deployment → Configuração para executar o flow em diferentes ambientes (local, nuvem, agendado, etc.).
+    #Agent → Quem executa os flows (pode rodar localmente, no Docker, Kubernetes, etc.).
+
+
